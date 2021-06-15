19f49d85a013d1d4c1f40fce4aed54aa2fe2f242
clevermantmnd <75678644+clevermantmnd@users.noreply.github.com>
2021-06-15 18:14:11 +0700

    Delete Fourth-Program.py

--- Fourth-Program.py
+++
@@ -1,7 +0,0 @@
-days = ['monday','tuesday','wednesday','thursday','sunday']
-
-for day in days:
-    if days.index(day) == len(days)-1:
-        print('I do not go to work on ' + day)
-    else:
-        print('I go to work on ' + day)
